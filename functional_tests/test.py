f73e5fed3dc0988c3ca167d06bd2173305b35c66
stamaimer <stamaimer@gmail.com>
2016-07-16 22:53:46 +0800

    modified:   functional_tests/test.py

--- functional_tests/test.py
+++ functional_tests/test.py
@@ -44,6 +44,10 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys("Buy peacock feathers")
 
         inputbox.send_keys(Keys.ENTER)
+        
+        edith_list_url = self.browser.current_url
+
+        self.assertRegexpMatches(edith_list_url, "/lists/.+")
 
         self.check_for_row_in_list_table("1: Buy peacock feathers")
 
@@ -56,6 +60,36 @@ class NewVisitorTest(LiveServerTestCase):
         self.check_for_row_in_list_table("1: Buy peacock feathers")
 
         self.check_for_row_in_list_table("2: Use peacock feathers to make a fly")
+
+        self.browser.quit()
+
+        self.browser = webdriver.Chrome()
+
+        self.browser.get(self.live_server_url)
+
+        page_text = self.browser.find_element_by_tag_name("body").text
+        
+        self.assertNotIn("Buy peacock feathers", page_text)
+
+        self.assertNotIn ("make a fly", page_text)
+
+        inputbox = self.browser.find_element_by_id("id_new_item")
+
+        inputbox.send_keys("Buy mild")
+
+        inputbox.send_keys(Keys.ENTER)
+
+        francis_list_url = self.browser.current_url
+
+        self.assertRegexpMatches(francis_list_url, "lists/.+")
+
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        page_text = self.browser.find_element_by_tag_name("body").text
+
+        self.assertNotIn("Buy peacock feathers", page_text)
+
+        self.assertIn("Buy milk", page_text)
 
         self.fail("Finish the test!")
 
